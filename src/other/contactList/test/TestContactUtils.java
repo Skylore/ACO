4c1d7a1653ba980fa9a251784b47d5c53023ae32
Skylore <iturchin98@gmail.com>
2016-11-16 16:30:08 +0200

    oop.week1.day2 tests

--- src/other/contactList/test/TestContactUtils.java
+++ src/other/contactList/test/TestContactUtils.java
@@ -24,6 +24,7 @@ public class TestContactUtils {
 
         Contact[] contacts = {contact1, contact2, contact3};
 
+        // Print res of tests
         System.out.println("Test add:");
         for (int i = 0; i < 3; i++) {
             System.out.println(testAdd(contacts[i]));
@@ -33,8 +34,13 @@ public class TestContactUtils {
         for (int i = 0; i < 3; i++) {
             System.out.println(testShowDetails(contacts[i]));
         }
+
+        System.out.println("\nTest show all:\n" + testShowAll(contact1, contact2, contact3));
+        System.out.println("\nTest override contact\n" + testOverrideContact(contact1));
+        System.out.println("\nTest remove contact\n" + testRemoveContact(contact1, contact2));
     }
 
+    // Tests
     private static boolean testAdd(Contact contact) {
         ContactList list = new ContactList();
         list.addContact(contact);
@@ -50,9 +56,39 @@ public class TestContactUtils {
         list.addContact(contact);
         String run = list.showDetails(1);
 
-        String expected = String.format("name - %s \nphone number - %s\nid - %d \noperator - %s",
-                contact.name, contact.phone,1 , contact.operator);
+        return run.contains(contact.name) && run.contains(contact.phone) && run.contains(contact.operator);
+    }
 
-        return run.equals(expected);
+    private static boolean testShowAll(Contact contact, Contact contact1, Contact contact2) {
+        ContactList list = new ContactList();
+        list.addContact(contact);
+        list.addContact(contact1);
+        list.addContact(contact2);
+
+        String run = list.showAll();
+
+        return  run.contains(contact.name) &&
+                run.contains(contact1.name) &&
+                run.contains(contact2.name);
+    }
+
+    private static boolean testOverrideContact(Contact contact) {
+        String testName = "Max";
+        String testPhone = "+3709423459871";
+
+        ContactList list = new ContactList();
+        list.addContact(contact);
+        list.overrideContact(1, testName, testPhone);
+
+        return contact.name.equals(testName) && contact.phone.equals(testPhone);
+    }
+
+    private static boolean testRemoveContact(Contact contact, Contact contact1) {
+        ContactList list = new ContactList();
+        list.addContact(contact);
+        list.addContact(contact1);
+        list.removeContact(1);
+
+        return list.getContacts()[0] != contact;
     }
 }
